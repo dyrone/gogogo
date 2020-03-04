c2db6d808909f489c8b569174baadf9875bc3a68
Dyrone Teng <dyroneteng@gmail.com>
2020-03-04 23:22:59 +0800

    Add context WithValue

---
+++ context-value/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	type favContextKey string
+
+	f := func(ctx context.Context, k favContextKey) {
+		if v := ctx.Value(k); v != nil {
+			fmt.Println("found value:", v)
+			return
+		}
+		fmt.Println("key not found:", k)
+	}
+
+	k := favContextKey("language")
+	ctx := context.WithValue(context.Background(), k, "Go")
+
+	f(ctx, k)
+	f(ctx, favContextKey("color"))
+
+	fmt.Println("test key 1:", ctx.Value("1"))
+	fmt.Println("test key k:", ctx.Value(k))
+}
