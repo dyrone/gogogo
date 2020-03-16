1467cf0688d989d766561bb1ad559f61765d80cb
tenglong.tl <dyroneteng@gmail.com>
2020-03-16 22:12:47 +0800

    Add bytes containsRune

---
+++ bytes-containsRune/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(bytes.ContainsRune([]byte("I like seafood."), 'f'))
+	fmt.Println(bytes.ContainsRune([]byte("I like seafood."), 'ö'))
+	fmt.Println(bytes.ContainsRune([]byte("去是伟大的!"), '大'))
+	fmt.Println(bytes.ContainsRune([]byte("去是伟大的!"), '!'))
+	fmt.Println(bytes.ContainsRune([]byte(""), '@'))
+
+	//./main.go:14:57: cannot use '\u5927' (type rune) as type []byte in argument to bytes.Contains
+	//fmt.Println(bytes.Contains([]byte("去是伟大的!"), '大'))
+}
