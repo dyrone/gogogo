13b729bbc78f115922d9c5c9a75d8995e82750e2
teng long <dyroneteng@gmail.com>
2020-04-05 23:38:42 +0800

    Add gogogo:
    	constants-iota
    
    Signed-off-by: teng long <dyroneteng@gmail.com>

---
+++ constants-iota/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type ByteSize float64
+
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	fmt.Printf("kb: %f", KB)
+}
