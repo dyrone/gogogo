33aba9f06bb6a3e487aec815d7b6202848e6de48
Dyrone Teng <dyroneteng@gmail.com>
2020-04-08 23:37:08 +0800

    Add cap slice
    Signed-off-by: Dyrone Teng <dyroneteng@gmail.com>

--- builtin/cap-something/main.go
+++ builtin/cap-something/main.go
@@ -6,4 +6,7 @@ func main() {
 	array := [10]int{1, 2, 3, 4, 5}
 	fmt.Printf("array cap: %d\n", cap(array))
 	fmt.Printf("array len: %d\n", len(array))
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Printf("slice cap: %d\n", cap(slice))
+	fmt.Printf("slice len: %d\n", len(slice))
 }
