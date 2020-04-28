2285ff7fc400ee47268258fbe27da3ed4c01fd06
Dyrone Teng <dyroneteng@gmail.com>
2020-04-28 23:03:41 +0800

    Add gogogo
    	* hex-decode
    	* hex-decodeString
    
    Signed-off-by: Dyrone Teng <dyroneteng@gmail.com>

---
+++ encoding/hex/decode/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "encoding/hex"
+import "log"
+
+func main() {
+	src := []byte("48656c6c6f20476f7068657221")
+
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	n, err := hex.Decode(dst, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", dst[:n])
+}
